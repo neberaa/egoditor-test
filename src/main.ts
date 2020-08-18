8c7ba19928b80b795df830054dd55fdec97e4e39
Nebera Anton <anebera@apextech.com.ua>
2020-08-18 22:36:53 +0300

    - add font-awesome icons

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import i18n from './i18n'
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
 
 Vue.config.productionTip = false;
 
